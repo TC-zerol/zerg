2a2443c29ed26b5624a46dfb984860d2dea07314
taocan <tao9can8@vip.qq.com>
2018-04-24 20:51:30 +0800

    first commit

---
+++ application/extra/wx.php
@@ -0,0 +1,21 @@
+
+<?php
+/**
+ * Created by 二虎哥哥.
+ * Author: 二虎哥哥
+ * QQ: 505120790
+ * Date: 2017/5/23
+ * Time: 13:52
+ */
+return [
+    'app_id' => 'wxaf0e3d7e0d07a041',
+    'app_secret' => 'e061086cb5ba67cb88f12af0bb4851ac',
+
+    // 微信使用code换取用户openid及session_key的url地址
+    'login_url' => "https://api.weixin.qq.com/sns/jscode2session?".
+        "appid=%s&secret=%s&js_code=%s&grant_type=authorization_code",
+
+    // 微信获取access_token的url地址
+    'access_token_url' => "https://api.weixin.qq.com/cgi-bin/token?" .
+        "grant_type=client_credential&appid=%s&secret=%s"
+];
